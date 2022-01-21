eb4ca0e7842848643e10dbf2bb0949bfbd244f9d
Sean King <sking@apvit.com>
2022-01-21 16:09:51 -0500

    increased max year"

--- acm-standard-applications/arkcase/src/main/webapp/resources/directives/date-time-picker/date-time-picker.client.directive.js
+++ acm-standard-applications/arkcase/src/main/webapp/resources/directives/date-time-picker/date-time-picker.client.directive.js
@@ -43,7 +43,7 @@ angular.module('directives').directive('dateTimePicker', ['moment', 'Util.DateSe
 
                 }
                 $scope.minYear = 1900;
-                $scope.maxYear = moment.utc($scope.dateInPicker).year() + 1;
+                $scope.maxYear = moment.utc($scope.dateInPicker).year() + 5;
             };
 
             $scope.setDate($scope.data);
