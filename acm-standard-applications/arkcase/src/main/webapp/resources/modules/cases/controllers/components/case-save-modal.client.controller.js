bdec97ff29eb27eedb755b0ed512a5df7f5e628f
Sean King <sking@apvit.com>
2022-03-10 16:37:08 -0500

    wip

---
+++ acm-standard-applications/arkcase/src/main/webapp/resources/modules/cases/controllers/components/case-save-modal.client.controller.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('cases').controller(
+        'Cases.CaseSaveModalController',
+        [ '$scope',
+        '$http',
+        '$stateParams',
+        '$translate',
+        '$modalInstance',
+        'Complaint.InfoService',
+        '$state',
+        'Object.LookupService',
+        'MessageService',
+        'UtilService',
+        '$modal',
+        'ConfigService',
+        'ObjectService',
+        'modalParams',
+        'Case.InfoService',
+        'Object.ParticipantService',
+        'Admin.FormWorkflowsLinkService',
+                function($scope, $http, $stateParams, $translate, $modalInstance, ComplaintInfoService, $state, ObjectLookupService, MessageService, Util, $modal, ConfigService, ObjectService, modalParams, CaseInfoService, ObjectParticipantService, AdminFormWorkflowsLinkService) {
+
+
+                    console.log('modalParams: ' + JSON.stringify(modalParams));
+                    $scope.modalParams = modalParams;
+
+                    var caseInfo = Util.omitNg($scope.modalParams.info);
+                    CaseInfoService.saveCaseInfo(caseInfo).then(function(caseInfo) {
+                        MessageService.info("Case Details Saved.");
+                        $modalInstance.close(caseInfo);
+                    });
+
+
+
+                } ]);
