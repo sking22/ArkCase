b780f29e117c6afd6acd59de68e1918ea3f8449b
ManizhaNizami <mnizami@apvit.com>
2023-03-30 15:45:22 -0400

    disable button for associate bot created

--- acm-standard-applications/arkcase/src/main/webapp/resources/modules/dashboard/controllers/components/associate-bot.client.controller.js
+++ acm-standard-applications/arkcase/src/main/webapp/resources/modules/dashboard/controllers/components/associate-bot.client.controller.js
@@ -1,20 +1,25 @@
 'use strict';
 
-angular.module('dashboard.associate-bot').config(function($httpProvider) {
-        $httpProvider.defaults.xsrfTrustedOrigins = ['https://acmsrpa.apvit.com'];
-})
-.controller('Dashboard.AssociateBotBtn', ['$scope', 'config', '$translate', 'Dashboard.DashboardService', 'ConfigService', 'params', 'UtilService', '$http',
+angular.module('dashboard.associate-bot').controller('Dashboard.AssociateBotBtn', ['$scope', 'config', '$translate', 'Dashboard.DashboardService', 'ConfigService', 'params', 'UtilService', '$http',
     function($scope, config, $translate, DashboardService, ConfigService, params, Util, $http) {
-        $scope.count = "click the button";
+        $scope.associateBotInfoText = "Click the button below to run the associate bot";
+        $scope.btnDisabled = false;
 
         //function for calling the associate bot
         $scope.runBotFunc = function() {
-            $http.get('https://acmsrpa.apvit.com:8080/callBatch')
-                .then(function(response) {
+            $scope.btnDisabled = true;
+            $http.get('https://acmsrpa.apvit.com:8080/callBatch',
+             { headers: {
+              'Access-Control-Allow-Origin': 'https://acmsrpa.apvit.com/',
+               'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                'Access-Control-Allow-Credentials': true, }
+              }).then(function(response) {
                     console.log('Endpoint called successfully');
-                })
+                    $scope.associateBotInfoText = "Associate bot run successfully";
+                 })
                 .catch(function(error) {
                     console.error('Error calling endpoint:', error);
+                    $scope.associateBotInfoText = "Error..... \n Associate bot did not run. Contact Support";
                 });
         };
     }
